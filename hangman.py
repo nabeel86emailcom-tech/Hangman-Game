d53faa8ef115ca9110b41c528f4dfba93e667a86
Nabeel Ul Rehman <nabeel86email.com@gmail.com>
2025-11-23 16:56:11 +0500

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,34 @@
+import random
+from wordlist import my_list
+from personimage import list
+from hangman_logos import logos
+
+print(random.choice(logos))
+choosed=random.choice(my_list)
+new_list=["_"]*len(choosed)
+print(new_list)
+chances=6
+gameover=False
+while not gameover:
+    guesslatter=input(" 🤔 Guess a latter:")
+
+    for position in range(len(choosed)):
+        latter=choosed[position]
+        if guesslatter==choosed[position]:
+            new_list[position]= latter
+            print(new_list)
+
+    if "_" not in new_list:
+        print("You Won...Congratulations You Saved A Life ")
+        gameover=True
+
+    if guesslatter not in choosed:
+        chances -= 1
+        print(f"⚠️ You Have Only {chances} More Chances To Save A Life")
+        if chances== 0:
+            print("You Kill A Person 🤦‍♂️")
+            gameover=True
+            
+print(list[chances])
+print(f"Word is {choosed}")      
+print(f"Your guess is {new_list}")
